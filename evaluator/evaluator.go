0b39ca3c4022c354251bdc2b62b141e7d08fcbb8
tbonelee <chanho0325@yonsei.ac.kr>
2024-04-19 21:55:50 +0900

    feat: Minus prefix operator evaluation 추가

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -38,6 +38,8 @@ func evalPrefixExpression(operator string, right object.Object) object.Object {
 	switch operator {
 	case "!":
 		return evalBangOperatorExpression(right)
+	case "-":
+		return evalMinusPrefixOperatorExpression(right)
 	default:
 		return NULL
 	}
@@ -54,6 +56,15 @@ func evalBangOperatorExpression(right object.Object) object.Object {
 	default:
 		return FALSE
 	}
+}
+
+func evalMinusPrefixOperatorExpression(right object.Object) object.Object {
+	if right.Type() != object.INTEGER_OBJ {
+		return NULL
+	}
+
+	value := right.(*object.Integer).Value
+	return &object.Integer{Value: -value}
 }
 
 func nativeBoolToBooleanObject(input bool) *object.Boolean {
