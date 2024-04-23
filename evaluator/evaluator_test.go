98c29b6a82dcdb07d1ab5c9898d94235579c83ef
tbonelee <chanho0325@yonsei.ac.kr>
2024-04-24 00:25:21 +0900

    feat: String concatenation 추가

--- evaluator/evaluator_test.go
+++ evaluator/evaluator_test.go
@@ -217,6 +217,10 @@ func TestErrorHandling(t *testing.T) {
 			"unknown operator: BOOLEAN + BOOLEAN",
 		},
 		{
+			`"Hello" - "World"`,
+			"unknown operator: STRING - STRING",
+		},
+		{
 			`
 if (10 > 1) {
 	if (10 > 1) {
@@ -318,6 +322,18 @@ addTwo(2);`
 
 func TestStringLiteral(t *testing.T) {
 	input := `"Hello World!"`
+	evaluated := testEval(input)
+	str, ok := evaluated.(*object.String)
+	if !ok {
+		t.Fatalf("object is not String. got=%T (%+v)", evaluated, evaluated)
+	}
+	if str.Value != "Hello World!" {
+		t.Errorf("String has wrong value. got=%q", str.Value)
+	}
+}
+
+func TestStringConcatenation(t *testing.T) {
+	input := `"Hello" + " " + "World!"`
 	evaluated := testEval(input)
 	str, ok := evaluated.(*object.String)
 	if !ok {
